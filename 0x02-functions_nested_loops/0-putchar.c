1542d63ebbba1f7604284fe96c4e1497a97c4dfb
Eroms01 <eromsyakubu@gmail.com>
2022-09-13 05:05:57 -0700

    1st commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - entry point
  * Return: always return 0
  */
 int main(void)
 {
-	printf("_putchar\n")
-	Return(0)
+	printf("_putchar\n");
+	Return(0);
 }
