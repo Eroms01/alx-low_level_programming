7b123de3373f31b4afe76f994e15731cec84e192
Eroms01 <eromsyakubu@gmail.com>
2022-09-30 15:17:05 -0700

    3rd commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * #include <stdio.h>
+ * @argc: input
+ * @argv: input
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
