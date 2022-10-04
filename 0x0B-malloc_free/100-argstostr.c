9d44b70d14c59fcb24b7cecd89618b9a06ffe4dc
Eroms01 <eromsyakubu@gmail.com>
2022-10-04 15:09:57 -0700

    6th commit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,46 +1,47 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * argstostr - gives array of args
- * @ac: argument count
- * @av: array of arguments
- * Return: char value
+ * argstostr - Concatenates all the arguments of the program
+ * @ac: Argument total count
+ * @av: Pointer to arguments
+ * Return: Pointer to concatenated string (SUCCESS) or
+ * NULL if @ac == 0 or @av == NULL (FAILURE) or
+ * NULL if if insufficient memory was available (FAILURE)
  */
 char *argstostr(int ac, char **av)
 {
-	int size;
-	char *s;
-	int i;
-	int j;
-	int k;
+	int i, j;
+	int count = 0;
+	int t_count = 0;
+	char *result;
 
 	if (ac == 0 || av == NULL)
-		return (NULL);
+		return ('\0');
 
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
-			size++;
+			t_count++;
+
+		t_count++;
 	}
 
-	size += (ac + 1);
-	s = malloc(sizeof(char) * size);
-	if (s == NULL)
-		return (NULL);
-	k = 0;
+	result = malloc(sizeof(char) * t_count + 1);
+
+	if (result == NULL)
+	{
+		return ('\0');
+	}
+
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			s[k] = av[i][j];
-			k++;
+			result[count++] = av[i][j];
 		}
-		s[k] = '\n';
-		k++;
+		result[count++] = '\n';
 	}
 
-	s[k] = '\0';
-
-	return (s);
+	result[t_count] = '\0';
+	return (result);
 }
